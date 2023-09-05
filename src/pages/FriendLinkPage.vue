304b400480ae9f7170c85ba035fd4919ee56761b
sdadgz <pengchengma5@gmail.com>
2023-09-05 21:37:19 +0800

    友链+1

--- src/pages/FriendLinkPage.vue
+++ src/pages/FriendLinkPage.vue
@@ -6,20 +6,22 @@
 
     <div class="row">
       <div class="col-3" v-for="friendLink in friendLinkList">
-        <q-card class="q-ma-md">
-          <q-card-section>
-            <q-avatar>
-              <q-img :src="friendLink.avatar"/>
-            </q-avatar>
-            {{ friendLink.title }}
-          </q-card-section>
-          <q-card-section>
-            <q-img :src="friendLink.image"/>
-          </q-card-section>
-          <q-card-section>
-            {{ friendLink.description }}
-          </q-card-section>
-        </q-card>
+        <a class="text-black" :href="friendLink.url" style="text-decoration: none">
+          <q-card class="q-ma-md cursor-pointer">
+            <q-card-section>
+              <q-avatar>
+                <q-img :src="friendLink.avatar"/>
+              </q-avatar>
+              {{ friendLink.title }}
+            </q-card-section>
+            <q-card-section>
+              <q-img :src="friendLink.image"/>
+            </q-card-section>
+            <q-card-section>
+              {{ friendLink.description }}
+            </q-card-section>
+          </q-card>
+        </a>
       </div>
     </div>
 
@@ -31,11 +33,11 @@
 import Header from "../components/public/Header.vue";
 
 const friendLinkList = [
-  {title: '', description: '', avatar: '', image: ''},
-  {title: '', description: '', avatar: '', image: ''},
-  {title: '', description: '', avatar: '', image: ''},
-  {title: '', description: '', avatar: '', image: ''},
-  {title: '', description: '', avatar: '', image: ''},
+  {title: 'wmh’s wiki', description: '鸟要高飞先振翅，人求上进先读书。', avatar: 'https://blog-1306153177.cos.ap-beijing.myqcloud.com/202308151203522.png', image: 'https://blog-1306153177.cos.ap-beijing.myqcloud.com/202308151204118.png', url: 'https://wmhwiki.cn/'},
+  {title: '', description: '', avatar: '', image: '', url: ''},
+  {title: '', description: '', avatar: '', image: '', url: ''},
+  {title: '', description: '', avatar: '', image: '', url: ''},
+  {title: '', description: '', avatar: '', image: '', url: ''},
 ]
 
 </script>
